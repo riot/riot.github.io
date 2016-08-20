2611d1ec0b10996c0d500fadcef3d70bfe338860
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-08-20 20:51:38 +0200

    updated: observable api

--- import.sh
+++ import.sh
@@ -2,12 +2,12 @@
 SUBMODULES=(route observable compiler)
 
 # clone the riot submodules in a temporary folder
-function clone {
+function _clone {
   git clone git@github.com:riot/$1.git tmp/$1
 }
 
 # update the doc files merging the doc files from each single submodule
-function update {
+function _update {
   tmp_file=api/$1-tmp.md
   # take the header from the old api file
   sed -n -e  "1,/{% include api-tabs.html %}/w $tmp_file" api/$1.md
@@ -31,8 +31,8 @@ function update {
 
 # loop all the submodules
 for sumbodule in ${SUBMODULES[@]}; do
-  clone ${sumbodule}
-  update ${sumbodule}
+  _clone ${sumbodule}
+  _update ${sumbodule}
 done
 
 # remove the temporary folder
